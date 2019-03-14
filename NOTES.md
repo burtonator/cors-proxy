a756fbe52761487622f2d82186e55a53f2346bb8
burtonator <burtonator@gmail.com>
2019-03-14 16:43:24 -0700

    this works now...

---
+++ NOTES.md
@@ -0,0 +1,6 @@
+
+this is a much better impl...
+
+
+https://github.com/rioam2/cors-reverse-proxy/
+https://github.com/Rob--W/cors-anywhere
--- functions/index.js
+++ functions/index.js
@@ -1,55 +1,91 @@
-'use strict';
-
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
-
 const fetch = require('node-fetch');
 
-const blockedPhrases = new RegExp(/porn|sexy/);  // No thank you.
+// Define whitelisted hostnames here to prevent abuse. Use '*'
+// to whitelist all hostnames (not recommended):
+const origin_whitelist = ['*'];
+const endpoint_whitelist = ['*'];
 
-/**
- * Returns the response body of the requested url, url should be encoded with encodeURIComponent if there are additional
- * parameters for the requested url.
- *
- * Example request using URL query parameters:
- *   https://us-central1-<project-id>.cloudfunctions.net/cors?url=https%3A%2F%2Fapi.ipify.org%3Fformat%3Djson
- * Example request using request body with cURL:
- *   curl -H 'Content-Type: application/json' \
- *        -d '{"url": "https://api.ipify.org/?format=json"}' \
- *        https://us-central1-<project-id>.cloudfunctions.net/cors
- *
- * This endpoint supports CORS.
- */
 exports.cors = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    console.log('Query:', req.query);
-    console.log('Body:', req.body);
+    cors(req, res, () => {
+        // Grab URL from URI or Req. Body:
+        let url = !req.query.url ? req.body.url : req.query.url;
+        if (!url) {
+            res.status(403).send('Endpoint URL not specified.');
+            return;
+        }
 
-    let url = req.query.url;
+        // Validate request origin and destination endpoints with whitelist:
+        const regexHostname = /(?:http(?:s)?:\/\/)?(((\S+)(?:.(com|net|edu|org|app)|:\d+)))/;
+        const reqOrigin = (req.headers.origin || req.headers.host).match(
+            regexHostname
+        )[1];
+        const reqDest = url.match(regexHostname)[1];
+        console.log(reqOrigin, reqDest);
+        if (
+            (!endpoint_whitelist.includes('*') &&
+                !endpoint_whitelist.includes(reqDest)) ||
+            (!origin_whitelist.includes('*') && !origin_whitelist.includes(reqOrigin))
+        ) {
+            // Send 403: Forbidden if endpoint or host are not in whitelists:
+            res.status(403).send('Forbidden.');
+            return;
+        }
 
-    if (!url) {
-      url = req.body.url;
-    }
+        // Add queries back to url:
+        Object.keys(req.query).forEach(query => {
+            if (query === 'url') return; // Skip url query. Already added as base
+            // If control reaches this point, query is not url, add to request uri
+            url += `&${query}=${decodeURI(req.query[query])}`;
+        });
 
-    if (!url) {
-      res.status(403).send('URL is empty.');
-    }
+        // TODO: cookies?
 
-    console.log('Request:', url);
+        // Define source request headers to retain in transmit request.
+        const headersKeep = [
+            'accept-encoding',
+            'accept-language',
+            'authorization',
+            'content-security-policy',
+            'content-type',
+            'referrer-policy',
+            'x-frame-options'
+        ];
 
-    // disallow blocked phrases
-    // if (url.match(blockedPhrases)) {
-    //   res.status(403).send('Phrase in URL is disallowed.');
-    // }
+        // Iterate through headers and transfer kept keys to new header object.
+        const headers = Object.keys(req.headers)
+            .filter(key => headersKeep.includes(key))
+            .reduce((obj, key) => {
+                obj[key] = req.headers[key];
+                return obj;
+            }, {});
 
-    fetch(url, {
-      method: req.method,
-      body: req.get('content-type') === 'application/json' ? JSON.stringify(req.body) : req.body,
-      headers: {
-        'Content-Type': req.get('Content-Type'),
-      },
-    })
-    .then(r => r.headers.get('content-type') === 'application/json' ? r.json() : r.text())
-    .then(body => res.status(200).send(body));
-  });
+        const opts = {
+            method: req.method,
+            headers
+        };
+
+        if (! ["GET", "HEAD"].includes(req.method)) {
+            opts.body = req.get('content-type') === 'application/json'
+                ? JSON.stringify(req.body)
+                : req.body;
+        }
+
+        // Send Interpreted Request to intended endpoint:
+        return fetch(url, opts).then(r => {
+            r.body.on('data', chunk => {
+                res.write(chunk);
+            });
+            return new Promise(resolve => {
+                r.body.on('end', () => {
+                    resolve(res.end(null));
+                });
+            });
+        }).catch(err => {
+            res.setHeader('x-proxied-request-failed', err.message);
+            res.setHeader('x-proxied-url', url);
+            res.status(500).send('Forbidden');
+        });
+    });
 });
